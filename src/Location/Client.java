be8de6902ad55706edc516ff2fa43e3f5dc817d6
Wolfhunter810 <yoann.auffredo@gmail.com>
2017-09-12 12:03:33 +0200

    Ecriture de la classe Client

--- src/Location/Client.java
+++ src/Location/Client.java
@@ -1,5 +1,81 @@
 package Location;
 
 public class Client {
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private int tel;
+	
+	public Client(String nom, String prenom, String adresse, int tel) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.tel = tel;
+	}
+	
+	/**
+	 * Returns the client's name
+	 * @return nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	
+	/**
+	 * Sets the client's name
+	 * @param nom
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	/**
+	 * Returns the client's first name
+	 * @return prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	/**
+	 * Sets the client's first name
+	 * @param prenom
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
 
+	/**
+	 * Returns the client's address
+	 * @return adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+	
+	/**
+	 * Sets the client's address
+	 * @param adresse
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	/**
+	 * Returns the client's phone number
+	 * @return tel
+	 */
+	public int getTel() {
+		return tel;
+	}
+
+	/**
+	 * Sets the client's phone number
+	 * @param tel
+	 */
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+	
 }
--- src/Location/Location.java
+++ src/Location/Location.java
@@ -80,5 +80,4 @@ public class Location {
 		this.client = client;
 	}
 	
-	
 }
